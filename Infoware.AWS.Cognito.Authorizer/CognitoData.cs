329274ae37a5443b932145ac9fef003f7afca0b5
Victor Sánchez <vicosanz@gmail.com>
2022-07-06 00:51:23 -0500

    CognitoData get set

--- Infoware.AWS.Cognito.Authorizer/CognitoData.cs
+++ Infoware.AWS.Cognito.Authorizer/CognitoData.cs
@@ -5,15 +5,15 @@ namespace Infoware.AWS.Cognito.Authorizer
 {
     public class CognitoData
     {
-        public string? UserId { get; internal set; }
-        public string? UserName { get; internal set; }
-        public List<string> Groups { get; internal set; } = new List<string>();
-        public string? ClientId { get; internal set; }
+        public string? UserId { get; set; }
+        public string? UserName { get; set; }
+        public List<string> Groups { get; set; } = new List<string>();
+        public string? ClientId { get; set; }
         [SensitiveData]
-        public string? AccessToken { get; internal set; }
-        public string? Email { get; internal set; }
+        public string? AccessToken { get; set; }
+        public string? Email { get; set; }
         [SensitiveData]
-        public string? IdToken { get; internal set; }
+        public string? IdToken { get; set; }
 
         public Guid UserIdGuid() => Guid.Parse(UserId ?? Guid.Empty.ToString());
     }
