448a6ae370f6380331fe91626227e84925537675
Victor Sánchez <vicosanz@gmail.com>
2022-12-06 00:52:29 -0500

    Additional information claims log

--- Infoware.AWS.Cognito.Authorizer/ApiGatewayAuthorizer/ApiGatewayAuthenticationHandler.cs
+++ Infoware.AWS.Cognito.Authorizer/ApiGatewayAuthorizer/ApiGatewayAuthenticationHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using Infoware.AWS.Cognito.Authorizer.OpenId;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Security.Claims;
@@ -28,6 +29,10 @@ namespace Infoware.AWS.Cognito.Authorizer.ApiGatewayAuthorizer
                 {
                     return Task.FromResult(AuthenticateResult.Fail("Couldn't find the user authenticated by API Gateway"));
                 }
+                var userId = Context.User.FindFirst(CognitoClaims.UserId);
+                var email = Context.User.FindFirst(CognitoClaims.Email);
+
+                Logger.LogInformation("UserId {id}, Email {email}", userId?.Value, email?.Value);
 
                 Logger.LogInformation("Found user already authenticated by API Gateway Authorizer");
                 var claimsIdentity = new ClaimsIdentity(Context.User.Claims, Scheme.Name);
